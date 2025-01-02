286721acc280a58b7b75aa5b7193a3da00c39953
MK <you@example.com>
2025-01-02 18:31:34 +0100

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -40,4 +40,14 @@ const IssueDetailPage = async (props: Props) => {
   );
 };
 
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt((await params).id) },
+  });
+  return {
+    title: issue?.title,
+    description: "Details of issue" + issue?.id,
+  };
+}
+
 export default IssueDetailPage;
